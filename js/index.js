85a39825193bb7132c3b53070921a9804310c7e6
Jakobi Matagi <jkmatagi@hotmail.com>
2021-01-12 14:03:04 -0700

    five out of ten done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+const enlargePhoto = document.querySelector('img');
+
+enlargePhoto.addEventListener("mouseover", function(event){
+    event.target.style.width = "75%";
+});
+
+const highlight = document.querySelector("h2");
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "Escape"){
+        highlight.style.color = "orange";
+    }
+});
+
+highlight.addEventListener("dblclick", function(event){
+    highlight.style.color = "black";
+})
+
+window.addEventListener('load', (event) =>{
+    console.log("Page is fully loaded.")
+})
+
+const alertHello = document.querySelector('h1');
+
+alertHello.addEventListener('click', function(){ alert("Hello")});
+
